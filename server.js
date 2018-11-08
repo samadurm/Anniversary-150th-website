f9d5f8a7dbd0812e2e824f07f88afedd3a8b0309
KorsK <kdong@my.chemeketa.edu>
2018-11-07 21:01:05 -0800

    reformat folders (server.js w.i.p)

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const http = require("http");
 const fs = require("fs");
 
-const css = fs.readFileSync("./public/style.css");
-const html = fs.readFileSync("./public/index.html");
-const js = fs.readFileSync("./public/index.js");
+var htmlFolder = fs.readdirSync("./public/html");
+var htmlContents = [];
+htmlFolder.forEach((file)=>{
+    htmlContents.push(fs.readFileSync("./public/html/" + file));
+});
+
+var cssFolder = fs.readdirSync("./public/css");
+var cssContents = [];
+cssFolder.forEach((file)=>{
+    cssContents.push(fs.readFileSync("./public/css/" + file));
+});
+
+var jsFolder = fs.readdirSync("./public/js");
+var jsContents = [];
+jsFolder.forEach((file)=>{
+    jsContents.push(fs.readFileSync("./public/js/" + file));
+});
 
 const portOptions = {
     hostname : 'localhost',
     port : 3000
 };
 
-http.createServer(function(req,res){
+http
+.createServer(function(req,res){
+
     if(req.url == "/"){
-        res.writeHead(200,"index.html header",{'Content-Type' : 'text/html'});
+        res.writeHead(200,"base html header",{'Content-Type' : 'text/html'});
+        var fileIndex;
+        htmlFolder.forEach((file)=>{
+
+        });
         res.write(html);
     }else if(req.url == "/style.css"){
         res.writeHead(200,"style header",{'Content-Type' : 'text/css'});
