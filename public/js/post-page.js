b98cd08c44160cdb89ad8b5848953831281f9624
unknown <samadurm@oregonstate.edu>
2018-11-29 20:36:14 -0800

    Logic for like/dislike buttons is WIP

--- public/js/post-page.js
+++ public/js/post-page.js
@@ -20,14 +20,44 @@ function initPost(callback = displayPosts){
         // thumbsUp.on('click', function() {
         //     console.log("thumbsUp is clicked");
         // });
+        var upClick = false;
         thumbsUp.addEventListener('click', function(event){
-            likes++;
-            likeCount.innerHTML = likes;
-            // thumbsUp.off('click');
+            if(upClick === false){
+                likes++;
+                likeCount.innerHTML = likes;
+                upClick = true;
+                thumbsUp.classList.toggle('highlight');
+            }
+            else{
+                upClick = false;
+                likes--;
+                likeCount.innerHTML = likes;
+                thumbsUp.classList.toggle('highlight');
+            }
         });
+        var downClick = false;
+        function handleThumbsDownClick() {
+            if(downClick === false){
+                dislikes++;
+                dislikeCount.innerHTML = dislikes;
+                downClick = true;
+                thumbsDown.classList.toggle('highlight');
+            }
+            else {
+                downClick = false;
+                dislikes--;
+                dislikeCount.innerHTML = dislikes;
+                thumbsDown.classList.toggle('highlight');
+            }
+        }
         thumbsDown.addEventListener('click', function(event){
-            dislikes++;
-            dislikeCount.innerHTML = dislikes;
+            if(upClick === false) {
+                handleThumbsDownClick();
+            }
+            else{
+                upClick = false;
+                handleThumbsDownClick();
+            }
         });
     };
     
@@ -43,72 +73,5 @@ function displayPosts(jsonInfo){
         document.getElementById('main-div').appendChild(image);
     });
 }
-// function comment() {
-// 	var lasttext=$("#TextArea").html();
-// 	var name=$("#commentator").val();
-//     var txt = $("#txt_pl").val();
-// 	var commenterName="<lable style=\"font-size:20px;\">"+name+":</lable>";
-// 	var commentContent="<p>"+txt+"</p>";
-//     lasttext += commenterName+commentContent+'<br/>';
-	
-//     $("#TextArea").html(lasttext);
-// }
 
-// //Good or bad evaluation
-// function Good(check) {
-//     var srctype = $("#good_img").attr("src");
-//     if (srctype.indexOf("good.png") > -1) {
-//         check = false;
-// 		if($("#bad_img").attr('src')=='Images/bad_orang.png'){
-// 			var badCount=$("#bad_count").text();
-// 			badCount--;
-// 			$("#bad_count").text(badCount);
-// 		}
-// 		$("#good_img").attr("src", "Images/good_orang.png");
-//         $("#bad_img").attr("src", "Images/bad.png");
-		
-// 		var goodCount=$("#good_count").text();
-// 		goodCount++;
-// 		$("#good_count").text(goodCount);
-		
-//     }
-//     else {
-//         check = true;
-//         $("#good_img").attr("src", "Images/good.png");
-
-		
-// 		var goodCount=$("#good_count").text();
-// 		goodCount--;
-// 		$("#good_count").text(goodCount);
-//     }
-
-// }
-
-// ////Good or bad evaluation
-// function Bad(check) {
-//     var srctype = $("#bad_img").attr("src");
-//     if (srctype.indexOf("bad.png") > -1) {
-//         check = false;
-// 		if($("#good_img").attr('src')=='Images/good_orang.png'){
-// 			var goodCount=$("#good_count").text();
-// 			goodCount--;
-// 			$("#good_count").text(goodCount);
-// 		}
-//         $("#bad_img").attr("src", "Images/bad_orang.png");
-//         $("#good_img").attr("src", "Images/good.png");
-		
-// 		var badCount=$("#bad_count").text();
-// 		badCount++;
-// 		$("#bad_count").text(badCount);
-//     }
-//     else {
-//         check = true;
-//         $("#bad_img").attr("src", "Images/bad.png");
-       
-// 		var badCount=$("#bad_count").text();
-// 		badCount--;
-// 		$("#bad_count").text(badCount);
-//     }
-
-// }
 
