9e9916722839f5ff4fffca240dfa05b8b685d64f
Matthew Mazon <mazonm@oregonstate.edu>
2018-12-03 16:25:14 -0800

    Added Search Ability

--- public/js/search-page.js
+++ public/js/search-page.js
@@ -24,5 +24,98 @@ function initSearch(id = null){
     		});
     	}
 
+      lookForInput(imgData);
+
+
+}
+
+
+function lookForInput (imgData) {
+
+  var counter = 0;
+
+  var valueText = document.getElementById('input-search').value;
+
+  valueText = valueText.toLowerCase();
+
+  for ( var i = 0; i < imgData.length; i++ ) {
+
+    var author = imgData[i].creator;
+
+    author = author.toLowerCase();
+
+    var title = imgData[i].title;
+
+    title = title.toLowerCase();
+
+    if ( (author.includes(valueText) == true) || (title.includes(valueText) == true) ){
+
+      author = imgData[i].creator;
+      id = imgData[i].id;
+      data = imgData[i].data;
+      title = imgData[i].title;
+      goodRating = parseInt(imgData[i].goodRating);
+      badRating = parseInt(imgData[i].badRating);
+
+      newPhotoHTML = makeNewPost ( author, id, data, title, goodRating, badRating );
+      var insertIntoDOM = document.getElementById ('searched-values');
+      insertIntoDOM.insertAdjacentHTML('beforeend', newPhotoHTML);
+
+      counter++;
+
+    }
+
+  }
+
+  if ( counter === 0 ) {
+
+    var div = document.createElement("div");
+
+    div.classList.add("err-div-first");
+
+    var newContent = document.createTextNode(" No Posts Found ");
+
+    div.appendChild(newContent);
+
+    var secondTwo = document.createElement("div");
+
+    secondTwo.classList.add("err-div-second");
+
+    var newerContent = document.createTextNode("Please Try Again or Press The All Posts Tab To See Posts");
+
+    secondTwo.appendChild(newerContent);
+
+    var insertIntoDOM = document.getElementById ('searched-values');
+
+    insertIntoDOM.appendChild (div);
+
+    insertIntoDOM.appendChild(secondTwo);
+
+  }
+
+}
+
+
+function makeNewPost ( author, id, data, title, goodRating, badRating ) {
+
+  var postTemplateToPost = Handlebars.templates.post;
+
+  var newPhotoHTML = postTemplateToPost ({
+
+    creator: author,
+
+    data: data,
+
+    id: id,
+
+    title: title,
+
+    goodRating: goodRating,
+
+    badRating: badRating
+
+  });
+
+  return newPhotoHTML;
 
 }
