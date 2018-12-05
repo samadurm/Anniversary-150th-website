2ae8abedf3a8520d9ec89a92c80010fe9868c064
unknown <samadurm@oregonstate.edu>
2018-12-04 16:36:27 -0800

    added commented out code which still doesnt grab json data

--- public/js/post-page.js
+++ public/js/post-page.js
@@ -1,5 +1,6 @@
 function initPost(){
         // Micah added this
+
         var likes = 0;
         var dislikes = 0;
         var thumbsUp = document.getElementById('thumbs-up');
@@ -59,8 +60,56 @@ function initPost(){
         postContainer.addEventListener('click', function(event){
             postContainer.classList.toggle('enlargePhoto');
         });
-        addEventListenersPost();
+        // var xmlhttp = new XMLHttpRequest();
+
+        // xmlhttp.onreadystatechange = function() {
+        //     if (this.readyState == 4 && this.status == 200) {
+        //         alert("loaded image json");
+        //         xmlReq.open('GET','image-data.json',true);
+        //         xmlReq.send();
+                
+        //         // document.getElementById("demo").innerHTML = myArr[0];
+        //     }
+        //     else{
+        //         alert("error occured in loading image json");
+        //     }
+    
+        // };
+
+        // var xmlReq = new XMLHttpRequest();
+        // xmlReq.onreadystatechange = function() {}
+        // if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+        //     console.log("this loaded successfully");
+        //     var imageData = JSON.parse(this.responseText);
+        //     document.getElementsByClassName('likes').innerHTML = imageData.goodRating;
+        //     console.log("image goodRAting is " + imageData.goodRating);
+        //     document.getElementsByClassName('dislikes').innerHTML = imageData.badRating;
+        //     console.log("image badRating is " + imageData.badRating);
+           
+        // }
+        // else if(this.readyState == XMLHttpRequest.DONE && this.status == 404){
+        //     else{
+        //     alert("error occured in loading image json");
+        // }
+
+        // function addEventListenersThumbs() {
+        //     var thumbsUp = document.getElementsByClassName('thumbs-up-class');
+        //     var thumbsDown = document.getElementsByClassName('thumbs-down-class');
+        //     thumbsUp.addEventListener('click', function(){
+        //         imageData.goodRating += 1;
+        //     });
+        //     thumbsDown.addEventListener('click', function(){
+        //         imageData.badRating += 1;
+        //     });
+        // }
+    
+    // xmlReq.open('GET','image-data.json',true);
+    // xmlReq.send();
+    // addEventListenersThumbs();
+    addEventListenersPost();
 }
+
+
 
 function addEventListenersPost(){
     var posts = document.getElementsByClassName('drawing-post');
