87476f7b1165d425d30b82564e27e60bfe46ba02
Matthew Mazon <mazonm@oregonstate.edu>
2018-12-04 22:16:22 -0800

    Fixed

--- public/js/post.js
+++ public/js/post.js
@@ -3,9 +3,11 @@
 templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class = post-container >\r\n    <div id = author-name-container class = post-redirect-item>\r\n        <p id = author uid = "
+  return "<div class = post-container uid = "
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + ">"
+    + ">\r\n    <div id = author-name-container class = post-redirect-item uid = "
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + ">\r\n        <p id = author >"
     + alias4(((helper = (helper = helpers.creator || (depth0 != null ? depth0.creator : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"creator","hash":{},"data":data}) : helper)))
     + "</p>\r\n    </div>\r\n    <div id = drawing-container uid = "
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
