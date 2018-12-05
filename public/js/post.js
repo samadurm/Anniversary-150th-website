68eaa1bf12920b49285962ebcbb8644fe5bd12d2
KorsK <kdong@my.chemeketa.edu>
2018-12-04 20:20:25 -0800

    up

--- public/js/post.js
+++ public/js/post.js
@@ -3,21 +3,21 @@
 templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div id = post-container >\r\n    <div id = author-name-container>\r\n        <p class = author uid = "
+  return "<div class = post-container >\r\n    <div id = author-name-container class = post-redirect-item>\r\n        <p id = author uid = "
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + ">"
     + alias4(((helper = (helper = helpers.creator || (depth0 != null ? depth0.creator : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"creator","hash":{},"data":data}) : helper)))
-    + "</p>\r\n    </div>\r\n    <div id = drawing-container class = drawing-post uid = "
+    + "</p>\r\n    </div>\r\n    <div id = drawing-container uid = "
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + ">\r\n        <img src = "
+    + " class = post-redirect-item>\r\n        <img src = "
     + alias4(((helper = (helper = helpers.data || (depth0 != null ? depth0.data : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"data","hash":{},"data":data}) : helper)))
-    + ">\r\n    </div>\r\n    <div id = info-container>\r\n        <p class = title uid = "
+    + ">\r\n    </div>\r\n    <div id = info-container>\r\n        <p id = title uid = "
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + ">"
+    + " class = post-redirect-item>"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</p>\r\n        <span id = thumbs-container>\r\n            <i class = \"fa fa-thumbs-up\" id = thumbs-up></i>\r\n                <span id = like-count>"
+    + "</p>\r\n        <span id = thumbs-container>\r\n            <i class = \"fa fa-thumbs-up\"selected='false' ></i>\r\n                <span class = like-count>"
     + alias4(((helper = (helper = helpers.goodRating || (depth0 != null ? depth0.goodRating : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"goodRating","hash":{},"data":data}) : helper)))
-    + "</span>\r\n            <i class = \"fa fa-thumbs-down\" id = thumbs-down></i>\r\n                <span id = dislike-count>"
+    + "</span>\r\n            <i class = \"fa fa-thumbs-down\"selected='false'></i>\r\n                <span class = dislike-count >"
     + alias4(((helper = (helper = helpers.badRating || (depth0 != null ? depth0.badRating : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"badRating","hash":{},"data":data}) : helper)))
     + "</span>\r\n        </span>\r\n    </div>\r\n</div>\r\n";
 },"useData":true});
